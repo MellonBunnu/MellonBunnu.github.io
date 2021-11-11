98661b9c4cb29c070d1ed2788962e16da61c0798
MellonBunnu <93677766+MellonBunnu@users.noreply.github.com>
2021-11-11 11:22:01 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RunningSS {
+ public static void main(String[] args) {
+  Process p;
+  try {
+   String[] cmd = { "sh", "/home/adb/Documents/main.sh"};
+   p = Runtime.getRuntime().exec(cmd); 
+   p.waitFor(); 
+   BufferedReader reader=new BufferedReader(new InputStreamReader(
+    p.getInputStream())); 
+   String line; 
+   while((line = reader.readLine()) != null) { 
+    System.out.println(line);
+   } 
+  } catch (IOException e) {
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+  } catch (InterruptedException e) {
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+  }
+ }
+}
